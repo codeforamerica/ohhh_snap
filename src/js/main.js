55769399a691043030c628cb87d5003f0ad183c4
Anselm <ans@anselmbradford.com>
2013-04-08 14:17:21 -0700

    Added default path redirect

--- src/js/main.js
+++ src/js/main.js
@@ -32,7 +32,9 @@ var main = (function () {
 		submitBtn.addEventListener( "mousedown" , submitClicked , false );
 		retryBtn.addEventListener( "mousedown" , resetClicked , false );
 		
-		var path = "data/"+getParameterByName('quiz')+".json";
+		var param = getParameterByName('quiz');
+		if (!param) {param="defaultquiz"; insertParam('quiz', 'defaultquiz'); }
+		var path = "data/"+param+".json";
 		$.getJSON(path, function(response){
 		   JSON = response;
 		})
@@ -40,6 +42,31 @@ var main = (function () {
 		.success(function() { quizLoadedSuccess(); })
 		.error(function() { quizLoadedError(); })
 		.complete(function() { quizLoadedComplete(); });
+	}
+
+	// insert a parameter into the URL
+	function insertParam(key, value)
+	{
+	    key = escape(key); value = escape(value);
+
+	    var kvp = document.location.search.substr(1).split('&');
+
+	    var i=kvp.length; var x; while(i--) 
+	    {
+	    	x = kvp[i].split('=');
+
+	    	if (x[0]==key)
+	    	{
+	    		x[1] = value;
+	    		kvp[i] = x.join('=');
+	    		break;
+	    	}
+	    }
+
+	    if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+	    //this will reload the page, it's likely better to store this until finished
+	    document.location.search = kvp.join('&'); 
 	}
 	
 	// functions for JSON loading success
